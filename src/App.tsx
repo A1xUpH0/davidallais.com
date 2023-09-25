5fb07f0c94f39965429c5494fe4ca20333445c6a
A1xUpH0 <davidallais2001@gmail.com>
2023-09-25 15:59:38 +0200

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import "@/styles/fonts.css";
+
+
+export default function App() {
+    return (
+        <div className="relative z-0 h-full bg-red-500">
+            DAVID ALLAIS
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+import plugin from "tailwindcss/plugin";
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                "xs": "320px"
+            },
+            height: {
+                screen: [
+                    "100vh /* fallback for Opera, IE and etc. */", "100dvh"
+                ],
+            },
+            textShadow: {
+                sm: "0 1px 2px var(--tw-shadow-color)",
+                DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+                lg: "0 8px 16px var(--tw-shadow-color)",
+                p: "2px 2px 4px rgba(0, 0, 0, 1)"
+            }
+        },
+    },
+    plugins: [
+        require("tailwind-scrollbar"),
+        require("tailwindcss-animated"),
+        plugin(function({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    "text-shadow": (value) => ({
+                        textShadow: value,
+                    }),
+                },
+                { values: theme("textShadow") }
+            );
+        }),
+    ]
+};
